441e9721631ea1a72a1c3e6d89e4fb1d5aa7fda4
suhailskhan9 <suhailskhan9@gmail.com>
2026-07-05 00:12:34 +0530

    Implement CRUD endpoints for projects

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
+
+const projects = [];
+let nextProjectId = 1;
 
 app.get("/", (req, res) => {
     res.json({
@@ -15,13 +19,68 @@ app.get("/health", (req, res) => {
 })
 
 app.get("/projects", (req, res) => {
-    res.json({
-        message: "This is the projects endpoint"
-    })
+    res.json(projects)
 })
 
-app.get("/tasks", (req, res) => {
-    res.json({
-        message: "This is the tasks endpoint"
-    })
+app.get("/projects/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    console.log('Id:  ---->',id);
+    const project = projects.find(p => p.id === id);
+
+    if(!project) {
+        return res.status(404).json({
+            message: "Project Not Found"
+        })
+    }
+
+    res.json(project)
 })
+
+app.post("/projects", (req, res) => {
+    const projectName = req.body.projectName;
+
+    const project = {
+        id: nextProjectId++,
+        projectName: projectName
+    }
+
+    projects.push(project);
+
+    res.status(201).json(project);
+})
+
+app.patch("/projects/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const projectName = req.body.projectName;
+
+    const project = projects.find(p => p.id === id)
+
+    if(!project) {
+        return res.status(404).json({
+            message: "Project Not Found"
+        })
+    }
+
+    project.projectName = projectName;
+
+    res.json(project);
+})
+
+app.delete("/projects/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+ 
+    const index = projects.findIndex(p => p.id === id);
+     if(index === -1 ) {
+        return res.status(404).json({
+            message: "Project Not Found"
+        })
+    }
+
+    const deletedProj = projects.splice(index, 1);
+    res.json({message: "Project Deleted", project: deletedProj[0]});
+})
+
+
+
+export default app;
