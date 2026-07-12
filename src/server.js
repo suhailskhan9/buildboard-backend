1196f9db9306bb46117129581524a593cd03531b
suhailskhan9 <suhailskhan9@gmail.com>
2026-07-13 02:28:48 +0530

    start Express server only after successful database connection

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 import dotenv from 'dotenv';
-import app from './app.js';
-
 dotenv.config();
 
+import app from './app.js';
+import pool from './db/database.js';
+
+
 const PORT = process.env.PORT || 3000;
+
+try {
+    const res = await pool.query("SELECT NOW()")
+    console.log("connected to the database")
+}
+catch(err) {
+    console.log("Database connection failed", err)
+    process.exit(1)
+}
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
